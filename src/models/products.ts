718722fa78186fdad610ea8345abb41f43b2bfe6
crossmywhy <35642405+crossmywhy@users.noreply.github.com>
2020-07-05 10:51:37 -0600

    Trying to add new item
    However, after clicking "New Item" button, the page becomes totally white.

--- src/models/products.ts
+++ src/models/products.ts
@@ -2,21 +2,29 @@ export default {
   namespace: 'products',
   state: [
     { name: 'dva', 
-    id: 'dva',
-    createTime: new Date().toLocaleTimeString(),
-    creator: "A",
-    price: 0,
-    category: "AAA" },
+      id: 1,
+      createTime: new Date().toLocaleTimeString(),
+      creator: "A",
+      price: 0,
+      category: "AAA" },
     { name: 'antd', 
-    id: 'antd', 
-    createTime: new Date().toLocaleTimeString(),
-    creator: "B",
-    price: 0,
-    category: "AAA" },
+      id: 2, 
+      createTime: new Date().toLocaleTimeString(),
+      creator: "B",
+      price: 0,
+      category: "AAA" },
   ],
   reducers: {
     delete(state, { payload: id }) {
       return state.filter(item => item.id !== id);
     },
+
+    addNewItem(state, { payload: newItem}) {
+      const newItemWithId = { ...newItem, id: 'TODO'};
+      const nextData = state.concat(newItemWithId);
+      return {
+        data: nextData,
+      };
+    }
   },
 };
